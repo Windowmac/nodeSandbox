fa11b24d836652b243f8ef90ed3dec923e0e4812
McKendree Strommer <mckendree.strommer@gmail.com>
2021-11-29 14:07:36 -0800

    set up post route for localhost, added public directory, and polished html

--- server.js
+++ server.js
@@ -2,15 +2,19 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3030;
+const path = require('path');
 
 app.use(express.json());
 app.use(express.urlencoded( { extended: true }));
+app.use(express.static(path.join(__dirname + '/public')));
 
 app.get('/', (req, res) => {
-    res.send('./index.html').status(200);
+    res.status(200).sendFile('/index.html');
 });
 
-app.post('')
+app.post('/solve', (req, res) => {
+    res.status(200).json(req.body);
+})
 
 app.listen(PORT, () => {
     console.log('listening on: ', PORT);
